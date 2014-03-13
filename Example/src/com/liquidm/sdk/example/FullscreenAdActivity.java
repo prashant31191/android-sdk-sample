ebb5aeaee5fd917f4a9b108101272b6423554c0e
Marek Zdankiewicz <m.zdankiewicz@gecko-lab.com>
2014-03-13 10:59:15 +0100

    Simplify fullscren ad example.

--- Example/src/com/liquidm/sdk/example/FullscreenAdActivity.java
+++ Example/src/com/liquidm/sdk/example/FullscreenAdActivity.java
@@ -2,16 +2,20 @@ package com.liquidm.sdk.example;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Toast;
 
+import com.liquidm.sdk.Ad;
+import com.liquidm.sdk.AdListener;
 import com.liquidm.sdk.InterstitialAd;
 
-public class FullscreenAdActivity extends Activity {
+public class FullscreenAdActivity extends Activity implements AdListener {
 
 	// Enter your site token here
 	private static final String SITE_TOKEN = "TestTokn";
 
 	InterstitialAd interstitial;
+
+	boolean started;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +23,53 @@ public class FullscreenAdActivity extends Activity {
 		setContentView(R.layout.activity_fullscreen_ad);
 
 		interstitial = new InterstitialAd(this, SITE_TOKEN);
+		interstitial.setListener(this);
 	}
 
-	public void onLoadClick(View view) {
-		interstitial.loadAd();
-	}
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-	public void onShowClick(View view) {
-		if (interstitial.isReady()) {
-			interstitial.show();
-		} else if (!interstitial.isLoading()) {
+		if (!started) {
 			interstitial.loadAd();
+
+			started = true;
 		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (isFinishing()) {
+			interstitial.stopLoading();
+		}
+	}
+
+	@Override
+	public void onAdLoad(Ad ad) {
+		interstitial.show();
+	}
+
+	@Override
+	public void onAdFailedToLoad(Ad ad) {
+		Toast.makeText(this, getString(R.string.interstitial_load_failed), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onAdClick(Ad ad) {
+	}
+
+	@Override
+	public void onAdPresentScreen(Ad ad) {
+	}
+
+	@Override
+	public void onAdDismissScreen(Ad ad) {
+	}
+
+	@Override
+	public void onAdLeaveApplication(Ad ad) {
 	}
 
 }
